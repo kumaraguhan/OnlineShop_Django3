89bee7496716ff0f32f5559324ad98e224c51541
Guhan <guhan3697figmentmajesty@gmail.com>
2025-07-16 13:10:15 +0000

    Create Models, Views, and Templates for Products Catalog

--- myshop/settings.py
+++ myshop/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
 ]
 
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
