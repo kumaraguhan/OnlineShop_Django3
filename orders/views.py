436fea0168f378ec4611e9cce7f8537d440f0419
Guhan <guhan3697figmentmajesty@gmail.com>
2025-07-17 12:59:33 +0000

    Extend Adminintration site with Custom views
    Here Custom view was created for order details that extends Django admin site inbuilt templates

--- orders/views.py
+++ orders/views.py
@@ -30,3 +30,16 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
